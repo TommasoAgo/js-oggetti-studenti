bd19e0c55088f94081730a3a41bd1b6441292440
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-12 14:24:27 +0200

    Finito Seconda parte

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
 // Dare la possibilità all’utente, attraverso 3 prompt(),
 //  di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
@@ -43,10 +45,16 @@ var arrayStudenti = [
 
 // Lo chiudo dentro un ciclo for per vedere caso per caso
 for ( var i = 0; i < arrayStudenti.length; i++ ) {
+
+    // Adesso ho in una variabile ogni singolo oggetto dell'arrayStudenti
     thisStudente = arrayStudenti[i];
 
-
+    // Stampo nome e cognome di ogni singolo studente
+    console.log(thisStudente.nome);
+    console.log(thisStudente.cognome);
 }
+
+
 
 
     
