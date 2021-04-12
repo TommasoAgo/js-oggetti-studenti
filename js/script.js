3b8d222e2037f9ed5b6a65f2b590ed9f91dd7007
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-12 14:18:44 +0200

    Fatta prima parte

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(),
+//  di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// Creo un oggetto 'Studente'
+var studente = {
+    'nome' : 'Francesco',
+    'cognome' : 'Totti',
+    'eta' : 45
+}
+
+// Stampo in un ciclo for - in le caratteristiche di 'Studente'
+for ( var key in studente ) {
+    console.log(studente[key])
+}
+
+// Creo un Array 'Classe' che contiene gli oggetti 'Studenti'
+var arrayStudenti = [
+    {
+        // Studente 1 
+        'nome' : 'Groucho',
+        'cognome' : 'Marx',
+        'eta' : 60
+    },
+    {
+        // Studente 2
+        'nome' : 'Friedrich',
+        'cognome' : 'Nietzsche',
+        'eta' : 55    
+    },
+    {
+        // Studente 3
+        'nome' : 'Marcel',
+        'cognome' : 'Duchamp',
+        'eta' : 81
+    }
+];
+
+// Stampo in un ciclo for le proprietà 'nome' e 'cognome' dei singoli 'oggetti' 'Studenti'
+
+// Lo chiudo dentro un ciclo for per vedere caso per caso
+for ( var i = 0; i < arrayStudenti.length; i++ ) {
+    thisStudente = arrayStudenti[i];
+
+
+}
+
+
+    
